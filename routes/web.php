6716d251284e7b0d36390af26b000c71d2610222
danis24ti@mahasiswa.pcr.ac.id <danis24ti@mahasiswa.pcr.ac.id>
2025-10-02 10:38:55 +0700

    pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+
 
 Route::get('/pcr', function () {
    return ' selamat datang di website kampus pcr';
@@ -13,3 +16,9 @@ Route::get('/mahasiswa', function () {
 Route::get('/nama/{param1}', function ($param1) {
    return ' nama saya:'.$param1;
 });
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
+
