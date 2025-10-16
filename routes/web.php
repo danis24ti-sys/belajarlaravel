602a5f25e87d17714686e21f9fd55c3eaac2cf50
danis24ti@mahasiswa.pcr.ac.id <danis24ti@mahasiswa.pcr.ac.id>
2025-10-16 10:33:02 +0700

    p6
    p6danis

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\DashboardController;
+
 
 
 Route::get('/pcr', function () {
@@ -22,3 +25,6 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
+Route::resource('pelanggan', PelangganController::class);
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
